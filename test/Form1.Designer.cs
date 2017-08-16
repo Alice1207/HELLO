32af9525d8765816f1bdcea5c3a62a87f4b21511
Alice <alice.sm.wong@mail.foxconn.com>
2017-08-16 11:03:47 +0800

    add new item

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -44,18 +45,27 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(110, 162);
+            this.button1.Location = new System.Drawing.Point(128, 178);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(138, 54);
             this.button1.TabIndex = 1;
             this.button1.Text = "new button";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "NEW";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -69,6 +79,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
